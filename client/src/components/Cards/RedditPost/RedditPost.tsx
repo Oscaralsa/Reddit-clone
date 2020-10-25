87cadfa2073538ed420873de1f2ce037d7eb6266
Oscar Alonso <oscaralsa@unisabana.edu.co>
2020-10-25 16:53:51 -0500

    Body post added

--- client/src/components/Cards/RedditPost/RedditPost.tsx
+++ client/src/components/Cards/RedditPost/RedditPost.tsx
@@ -2,22 +2,29 @@ import React from "react";
 
 import { Col, Row } from "react-bootstrap";
 
+import RedditChar from "../../../assets/images/character.png";
+
 import "./RedditPost.scss";
 
-function RedditPost(){
-  return(
+function RedditPost(props: any) {
+  return (
     <div className="card-post">
       <Row>
         <Col xs={1} className="card-post__left">
           <p>Holsa</p>
         </Col>
         <Col xs={11} className="card-post__body">
-            <p>Reddit post</p>
-            <h6>The best Reddit experience, with monthly Coins</h6>
+          <div className="card-post__body__header">
+            <img src={RedditChar} alt="logo" />
+            <p className="card-post__body__header__user">reddit user</p>
+            <p className="card-post__body__header__separator">• Posted by user 10 hours ago</p>
+          </div>
+          <h5>The best Reddit experience, with monthly Coins</h5>
+          <h4>Here comes the textHere comes the textHere comes the textHere comes the textHere comes the textHere comes the textHere comes the textHere comes the text</h4>
         </Col>
       </Row>
     </div>
-  )
+  );
 }
 
 export default RedditPost;
