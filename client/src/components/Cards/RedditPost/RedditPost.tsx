b10ec630f087b9aedc8b50d38612fb831a9ddade
Oscar Alonso <oscaralsa@unisabana.edu.co>
2020-10-28 16:17:26 -0500

    Post style finished

--- client/src/components/Cards/RedditPost/RedditPost.tsx
+++ client/src/components/Cards/RedditPost/RedditPost.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 import { Col, Row } from "react-bootstrap";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowDown,
+  faArrowUp,
+  faComment,
+  faGift,
+  faSave,
+  faShare,
+} from "@fortawesome/free-solid-svg-icons";
 import RedditChar from "../../../assets/images/character.png";
 
 import "./RedditPost.scss";
@@ -11,16 +19,45 @@ function RedditPost(props: any) {
     <div className="card-post">
       <Row>
         <Col xs={1} className="card-post__left">
-          <p>Holsa</p>
+          <div className="card-post__left__buttons">
+            <span className="card-post__left__buttons__up">
+              <FontAwesomeIcon icon={faArrowUp} />
+            </span>
+            <div className="card-post__left__buttons__votes">122</div>
+            <span className="card-post__left__buttons__down">
+              <FontAwesomeIcon icon={faArrowDown} />
+            </span>
+          </div>
         </Col>
         <Col xs={11} className="card-post__body">
           <div className="card-post__body__header">
             <img src={RedditChar} alt="logo" />
             <p className="card-post__body__header__user">reddit user</p>
-            <p className="card-post__body__header__separator">• Posted by user 10 hours ago</p>
+            <p className="card-post__body__header__separator">
+              • Posted by user 10 hours ago
+            </p>
           </div>
           <h5>The best Reddit experience, with monthly Coins</h5>
-          <h4>Here comes the textHere comes the textHere comes the textHere comes the textHere comes the textHere comes the textHere comes the textHere comes the text</h4>
+          <h4>
+            Here comes the textHere comes the textHere comes the textHere comes
+            the textHere comes the textHere comes the textHere comes the
+            textHere comes the text
+          </h4>
+
+          <div className="card-post__body__footer">
+            <div className="card-post__body__footer__comments">
+              <FontAwesomeIcon icon={faComment} /><p>36 Comments</p>
+            </div>
+            <div className="card-post__body__footer__others">
+              <FontAwesomeIcon icon={faGift} /><p>Give Award</p>
+            </div>
+            <div className="card-post__body__footer__others__small">
+              <FontAwesomeIcon icon={faShare} /><p>Share</p>
+            </div>
+            <div className="card-post__body__footer__others__small">
+              <FontAwesomeIcon icon={faSave} /><p>Save</p>
+            </div>
+          </div>
         </Col>
       </Row>
     </div>
